864754a899bc54b93f69af565104de51287a455a
Rajeevanand07 <anandrajeev1234@gmail.com>
2025-04-07 21:18:15 +0530

    added vercel.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
+  // server: {
+  //   allowedHosts: [
+  //     "f84a-2405-201-500c-b0fd-dda7-97dd-c608-ec2d.ngrok-free.app",
+  //   ],
+  //   port: 5173,
+  //   open: true,
+  //   host: true,
+  // },
 
-  server: {
-    allowedHosts: [
-      "f84a-2405-201-500c-b0fd-dda7-97dd-c608-ec2d.ngrok-free.app",
-    ],
-    port: 5173,
-    open: true,
-    host: true,
-  },
 
 });
