eb5d4935f17bb23d204ec8994db5f9cfd8b02dfa
Rajeevanand07 <anandrajeev1234@gmail.com>
2025-04-08 15:11:55 +0530

    chnages in albums

--- backend/config/isAuthenicated.js
+++ backend/config/isAuthenicated.js
@@ -4,6 +4,8 @@ import jwt from "jsonwebtoken";
 export const isAuthenticated = (req, res, next) => {
   const token = req.cookies?.token;
 
+  console.log(token)
+
   if (!token) {
     return res.status(401).json({ success:false,message: "Unauthorized: No token provided" });
 
@@ -11,6 +13,7 @@ export const isAuthenticated = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded);    
     req.user = decoded; // Add decoded user data to the request object
     next();
   } catch (error) {
